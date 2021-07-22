35ab3e4c67d718579214be5272d9f446924b8a00
dimamarjan <dima.marjan@gmail.com>
2021-07-22 09:35:33 +0300

    done

--- src/App.js
+++ src/App.js
@@ -4,15 +4,25 @@ import { Filter } from "components/Filter/Filter";
 import { ContactList } from "components/ContactList/ContactList";
 import { v4 as uuidv4 } from "uuid";
 
-function App() {
-  const [contacts, setСontacts] = useState(() => {
-    return JSON.parse(window.localStorage.getItem("phonebook")) ?? [];
+const useLocalStorage = (key, defaultValue) => {
+  const [state, setState] = useState(() => {
+    return JSON.parse(window.localStorage.getItem(key)) ?? defaultValue;
   });
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(state));
+  }, [key, state]);
+
+  return [state, setState];
+};
+
+function App() {
+  const [contacts, setСontacts] = useLocalStorage("contacts", []);
+  const [filteredData, setFilteredData] = useState([]);
   const [filter, setFilter] = useState("");
   const [name, setName] = useState("");
   const [number, setNumber] = useState("");
 
-  const addContact = (event) => {
+  const handleAdd = (event) => {
     event.preventDefault();
     const { target } = event;
     const newId = uuidv4();
@@ -73,142 +83,35 @@ function App() {
   };
 
   useEffect(() => {
-    localStorage.setItem("phonebook", JSON.stringify(contacts));
-  });
+    if (filter) {
+      const isDatNumber = parseInt(filter);
+      if (isDatNumber) {
+        setFilteredData([
+          ...contacts.filter((contactItem) => {
+            return contactItem.number.indexOf(filter) > -1;
+          }),
+        ]);
+      } else {
+        setFilteredData([
+          ...contacts.filter((contactItem) => {
+            return contactItem.name.indexOf(filter) > -1;
+          }),
+        ]);
+      }
+    } else {
+      setFilteredData(contacts);
+    }
+  }, [filter, contacts]);
 
   return (
     <div>
       <h1>Phonebook</h1>
-      <ContactForm onHandleChange={handleChange} onAddContact={addContact} />
+      <ContactForm onHandleChange={handleChange} onAddContact={handleAdd} />
       <h2>Contacts</h2>
       <Filter onHandleChange={handleChange} />
-      <ContactList onDelete={handleDelete} filtredDataArr={contacts} />
+      <ContactList onDelete={handleDelete} filtredDataArr={filteredData} />
     </div>
   );
 }
 
 export default App;
-
-// export default class App extends Component {
-//   state = {
-//     contacts: [],
-//     filter: "",
-//     name: "",
-//     number: "",
-//   };
-
-//   handleChange = (event) => {
-//     const inputData = event.target.value;
-//     this.setState({
-//       [event.target.name]: inputData,
-//     });
-//   };
-
-//   addContact = (event) => {
-//     event.preventDefault();
-//     const newId = uuidv4();
-//     const { name, number, contacts } = this.state;
-//     let message = "";
-//     contacts.forEach((contactItem) => {
-//       if (contactItem.name === name) {
-//         message = `${name} is already in contacts.`;
-//         alert(message);
-//       }
-//       if (contactItem.number === number) {
-//         message = `${number} is already in contacts.`;
-//         alert(message);
-//       }
-//     });
-//     if (message.length === 0) {
-//       this.setState((prevState) => ({
-//         contacts: [
-//           ...prevState.contacts,
-//           { id: newId, name: name, number: number },
-//         ],
-//       }));
-//       event.target[0].value = "";
-//       event.target[1].value = "";
-//     } else {
-//       event.target[0].value = "";
-//       event.target[1].value = "";
-//     }
-//   };
-
-//   filterDataHandle = (event) => {
-//     const inputData = event.target.value;
-//     this.setState({
-//       filter: inputData,
-//     });
-//   };
-
-//   filterContacts = () => {
-//     const { contacts } = this.state;
-//     const stateFilterItem = this.state.filter;
-//     const isName = parseInt(stateFilterItem);
-//     if (isNaN(isName)) {
-//       const newArrData = [
-//         ...contacts.filter((contactItem) => {
-//           return (
-//             contactItem.name
-//               .toLowerCase()
-//               .indexOf(stateFilterItem.toLowerCase()) > -1
-//           );
-//         }),
-//       ];
-//       return newArrData;
-//     } else {
-//       const newArrData = [
-//         ...contacts.filter((contactItem) => {
-//           return contactItem.number.indexOf(stateFilterItem) > -1;
-//         }),
-//       ];
-//       return newArrData;
-//     }
-//   };
-
-//   deleteContact = (event) => {
-//     const { contacts } = this.state;
-//     contacts.forEach((contactElem, index) => {
-//       if (contactElem.id === event.target.value) {
-//         contacts.splice(index, 1);
-//       }
-//     });
-//     this.setState({
-//       contacts: contacts,
-//     });
-//     localStorage.setItem("phonebook", JSON.stringify(contacts));
-//   };
-
-//   componentDidUpdate(prevProps, { contacts }) {
-//     if (contacts !== this.state.contacts) {
-//       localStorage.setItem("phonebook", JSON.stringify(this.state.contacts));
-//     }
-//   }
-
-//   componentDidMount() {
-//     const localData = JSON.parse(localStorage.getItem("phonebook"));
-//     if (localData) {
-//       this.setState({
-//         contacts: localData,
-//       });
-//     }
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <h1>Phonebook</h1>
-//         <ContactForm
-//           onHandleChange={this.handleChange}
-//           onAddContact={this.addContact}
-//         />
-//         <h2>Contacts</h2>
-//         <Filter onHandleChange={this.filterDataHandle} />
-//         <ContactList
-//           onDelete={this.deleteContact}
-//           filtredDataArr={this.filterContacts()}
-//         />
-//       </div>
-//     );
-//   }
-// }
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,13 +1,13 @@
 import { LabelContacts, LabelContactsText, InputContacts, AccentText } from "components/Filter/Filter.style"
 
-export function Filter ({handleChange}) {    
+export function Filter ({onHandleChange}) {    
     return (
         <div>
             <LabelContacts>
                 <LabelContactsText>Find contacts by <AccentText>name</AccentText> or <AccentText>number</AccentText></LabelContactsText>
                 <InputContacts
                     name="filter"
-                    onChange={handleChange} />
+                    onChange={onHandleChange} />
             </LabelContacts>
         </div>
     );
